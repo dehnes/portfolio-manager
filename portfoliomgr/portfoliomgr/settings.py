9c76d3487ef5b07f64c6aeb3bfc6142c365a4c92
dehnes <dennbauer@gmail.com>
2024-06-14 14:23:06 +0200

    Bootstrap Template added

--- portfoliomgr/portfoliomgr/settings.py
+++ portfoliomgr/portfoliomgr/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_datta.apps.AdminDattaConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
