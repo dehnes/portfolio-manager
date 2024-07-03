32ed35e49fcb5aad13ba09e775770fb83f3932c7
dehnes <dennbauer@gmail.com>
2024-07-03 12:14:22 +0200

    bug fixing

--- portfoliomgr/portfolio/market.py
+++ portfoliomgr/portfolio/market.py
@@ -3,11 +3,11 @@ import pprint
 import yfinance as yf
 
 
-def get_market_price(symbol: str):
+def get_market_price(symbol: str = "tbd") -> float:
 
     if symbol == "tbd":
         return 0.0
     symbol = symbol + ".DE"
-    data = yf.Ticker(symbol).info
-    test = data["regularMarketPreviousClose"]
-    return test
+    data = yf.Ticker(symbol).fast_info
+    result = float(data["regularMarketPreviousClose"])
+    return result
