458b77f4e36f1311709eb19bf1d4231dcb87df6b
dehnes <dennbauer@gmail.com>
2024-07-03 13:38:54 +0200

    Created the detailed portfolio page

--- portfoliomgr/portfolio/urls.py
+++ portfoliomgr/portfolio/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("bankaccounts/", views.BankAccountsList.as_view(), name="bankaccounts"),
     path("portfolios/", views.portfolios, name="portfolios"),
+    path("portfolios/<int:id>", views.portfolio, name="portfolio"),
     path("deposit/", views.Deposit.as_view(), name="deposit"),
 ]
--- portfoliomgr/portfolio/views.py
+++ portfoliomgr/portfolio/views.py
@@ -25,14 +25,68 @@ from .models import (
 def index(request):
     context = {}
 
+    # Get all relevant data
     persons = Person.objects.all()
     securities = Security.objects.all()
+    portfolios = Portfolio.objects.all()
+    depots = Depot.objects.all()
+    assets = Asset.objects.annotate(
+        batch_positions_sum=(Sum("batch__batch_positions__quantity")),
+    )
+    # Calculating the Portfolio Values
+    for asset in assets:
+        if asset.batch_positions_sum > 0:
+            asset.price = get_market_price(asset.fk_security.ticker_symbol)
+            asset.balance = float(asset.price) * float(asset.batch_positions_sum)
+
+    for depot in depots:
+        depot.balance = 0.0
+    for port in portfolios:
+        port.balance = 0.0
+
+    for depot in depots:
+        for asset in assets:
+            if asset.fk_depot == depot:
+                depot.balance += asset.balance
+
+    for port in portfolios:
+        for depot in depots:
+            if depot.fk_portfolio == port:
+                port.balance += depot.balance
+
+    context["portfolios"] = portfolios
+
     for sec in securities:
         sec.price = get_market_price(sec.ticker_symbol)
 
     context["persons"] = persons
     context["securities"] = securities
     return render(request, "portfolio/index.html", context)
+
+
+def portfolio(request, id):
+    context = {}
+    port = Portfolio.objects.filter(id=id)[0]
+    depots = Depot.objects.filter(fk_portfolio=port)
+    assets = Asset.objects.annotate(
+        batch_positions_sum=(Sum("batch__batch_positions__quantity")),
+    )
+    for asset in assets:
+        if asset.batch_positions_sum > 0:
+            asset.price = get_market_price(asset.fk_security.ticker_symbol)
+            asset.balance = float(asset.price) * float(asset.batch_positions_sum)
+    for depot in depots:
+        depot.balance = 0.0
+    for depot in depots:
+        for asset in assets:
+            if asset.fk_depot == depot:
+                depot.balance += asset.balance
+
+    context["port"] = port
+    context["depots"] = depots
+    context["assets"] = assets
+
+    return render(request, "portfolio/portfolio.html", context)
 
 
 def portfolios(request):
