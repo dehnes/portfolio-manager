73c3c903e32bc0fea45d4014d66333fedc65a5a5
dba <dennbauer@gmail.com>
2024-09-18 10:43:30 +0200

    GitIgnore Modified

--- portfoliomgr/portfolio/market.py
+++ portfoliomgr/portfolio/market.py
@@ -7,7 +7,7 @@ def get_market_price(symbol: str = "tbd") -> float:
 
     if symbol == "tbd":
         return 0.0
-    symbol = symbol + ".DE"
+    symbol += ".DE"
     data = yf.Ticker(symbol).fast_info
     result = float(data["regularMarketPreviousClose"])
     return result
